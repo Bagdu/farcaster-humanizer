2860e262aacf2904da625e452b9ec8013ec7feea
3NFBagdu <gbagh16@freeuni.edu.ge>
2024-08-15 14:28:31 +0200

    feat: add fid verification

--- frontend/src/hooks/UseVerifyProof.ts
+++ frontend/src/hooks/UseVerifyProof.ts
@@ -5,10 +5,10 @@ import {useAccount} from "wagmi";
 
 export function useVerifyProof() {
   const {address} = useAccount();
-  const verifyProof = async (result: any, farcasterAppId: string) => {
+  const verifyProof = async (result: any, fid: string) => {
     const [unpackedProof] = decodeAbiParameters([{ type: 'uint256[8]' }], result.proof);
 
-    const args = [farcasterAppId, address, result.merkle_root, result.nullifier_hash, unpackedProof];
+    const args = [fid, result.merkle_root, result.nullifier_hash, unpackedProof];
 
     const config = await prepareWriteContract({
       address: addresses.verify,
@@ -21,13 +21,13 @@ export function useVerifyProof() {
     await waitForTransaction({ hash });
   };
 
-  const checkVerifyProof = async (appId: string, address: string) => {
+  const checkVerifyProof = async (fid: string) => {
 
     return await readContract({
       address: addresses.verify,
       abi: abis.verify,
       functionName: 'isVerified',
-      args: [appId, address]
+      args: [fid]
     });
   };
 
