a9c8bb1c350d1456dfc785b5f2cd8635b33caf9d
3NFBagdu <gbagh16@freeuni.edu.ge>
2024-09-09 23:44:43 +0200

    feat: change minor styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,30 +1,40 @@
 import { Container, Navbar, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 function Header() {
   return (
-    <Navbar
-      expand="lg"
-      sticky="top"
-      className="bg-body-tertiary bg-opacity-75"
-      style={{
-        backdropFilter: 'blur(10px)',
-      }}
-    >
-      <Container>
-        <div className="ms-0">
-          <Image src="/images/world-coin-logo.jpg" height={28} className="me-lg-3" />
-        </div>
-        <Navbar.Brand as={Link} to="/">
-          <Image src="/images/farcaster-logo.jpg" height={28} className="me-lg-5" />
-          Farcaster Humanizer
-        </Navbar.Brand>
-        <div className="ms-auto">
-          <w3m-button />
-        </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-2" />
-      </Container>
-    </Navbar>
+    <>
+      <Navbar
+        expand="lg"
+        sticky="top"
+        className="bg-body-tertiary bg-opacity-75"
+        style={{
+          backdropFilter: 'blur(10px)',
+        }}
+      >
+        <Container>
+          <div className="ms-0">
+            <Image src="/images/world-coin-logo.jpg" height={28} className="me-lg-3" />
+          </div>
+          <Navbar.Brand as={Link} to="/">
+            <Image src="/images/farcaster-logo.jpg" height={28} className="me-lg-5" />
+            Farcaster Humanizer
+          </Navbar.Brand>
+          <div className="ms-auto">
+            <w3m-button />
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-2" />
+        </Container>
+      </Navbar>
+      <hr style={{
+        border: 0,
+        display: "block",
+        width: "96%",
+        backgroundColor: "#FFFF00",
+        height: 1
+      }} />
+    </>
   );
 }
 
